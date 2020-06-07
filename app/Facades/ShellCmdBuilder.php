d1d2f90bae5f337653def83c33a863b9fed144c6
Edvinas <edvinas2121@hotmail.com>
2020-06-07 18:49:23 +0300

    added extra methods to cmdBuilder

--- app/Facades/ShellCmdBuilder.php
+++ app/Facades/ShellCmdBuilder.php
@@ -8,7 +8,14 @@ use Illuminate\Support\Facades\Facade;
 /**
 *@method static string gitPull(string $userFolder,string $projectFolder)
 *@method static string gitClone(string $userFolder,string $projectFolder,string $url)
-*@method static string folder(string $userName,?string $deleteFolder =null)
+*@method static string composerInstall(string $userFolder,string $projectFolder)
+*@method static string dbCreate(string $dbName)
+*@method static string appKeyGenerate(string $userFolder,string $projectFolder)
+*@method static string appStorageLink(string $userFolder,string $projectFolder)
+*@method static string dbMigrate(string $userFolder,string $projectFolder)
+*@method static string dumpAutoload(string $userFolder,string $projectFolder)
+*@method static string dbSeed(string $userFolder,string $projectFolder,?string $seedClass = null)
+*@method static string customArtisan(string $userFolder,string $projectFolder,string $command)
 *
 **/
 
--- app/Helpers/ShellCmdHelper.php
+++ app/Helpers/ShellCmdHelper.php
@@ -24,6 +24,7 @@ private $wwwRoute = 'cd /mnt/c/Users/Edvinas/shellOutputTest'; //just for testin
  {
     return $this->wwwRoute.'/'.$userFolder.' && git clone '.$url.' '.$projectFolder.' 2>&1';
  }
+
  public function folder(string $userName,?string $deleteFolder =null): string
  {
     if($deleteFolder === null){
@@ -32,5 +33,55 @@ private $wwwRoute = 'cd /mnt/c/Users/Edvinas/shellOutputTest'; //just for testin
         return $this->wwwRoute.'/'.$userName.' && rm -r '.$deleteFolder;
     }
  }
+
+ public function composerInstall(string $userFolder,string $projectFolder): string
+ {
+    return $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && composer install 2>&1';
+ }
+
+ public function dbCreate(string $dbName):string
+ {
+    //  return 'mysql -upi -ptest -e "create database cmdTest1"';
+    return 'mysql -u'.env('DB_USERNAME','root').' -p'.env('DB_PASSWORD','').' -e "create database '.$dbName.'"';
+
+ }
+
+ public function appKeyGenerate(string $userFolder,string $projectFolder):string
+ {
+     return $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan key:generate 2>&1';
+ }
  
+ public function appStorageLink(string $userFolder,string $projectFolder):string
+ {
+     //FILESYSTEM_DRIVER=public should be added to .env file before runing this command
+     return $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan storage:link 2>&1';
+ }
+
+ public function dbMigrate(string $userFolder,string $projectFolder):string
+ {
+     return $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan migrate 2>&1';
+ }
+
+ public function dumpAutoload(string $userFolder,string $projectFolder):string
+ {
+     return $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan migrate 2>&1';
+ }
+ public function dbSeed(string $userFolder,string $projectFolder,?string $seedClass = null):string
+ {
+     if($seedClass && preg_match('/^[a-zA-Z0-9]+$/',$seedClass)){
+         return  $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan db:seed --class='.$seedClass.' 2>&1';
+     }
+     if($seedClass === null){
+        return  $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan db:seed 2>&1';
+     }
+     return '';
+ }
+
+ public function customArtisan(string $userFolder,string $projectFolder,string $command):string
+ {
+     if(!preg_match('/[&|;]+/', $command)){
+         return $this->wwwRoute.'/'.$userFolder.'/'.$projectFolder.' && php artisan '.$command.' 2>&1';
+     }
+     return '';
+ }
 }
