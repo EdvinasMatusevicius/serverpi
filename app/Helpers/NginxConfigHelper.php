5a886fb91c3349ab8eb3b926f0b1af2ad8279c58
Edvinas <edvinas2121@hotmail.com>
2020-09-17 19:18:02 +0300

    no nginx restart on app delete

--- app/Helpers/NginxConfigHelper.php
+++ app/Helpers/NginxConfigHelper.php
@@ -63,7 +63,7 @@ class NginxConfigHelper
 
         }
         public function deleteNginxConfig($project){
-                return "cd /etc/nginx/sites-available && rm '{$project}'.conf && cd /etc/nginx/sites-enabled &&  rm '{$project}'.conf && sudo /usr/sbin/service nginx restart";
+                return "cd /etc/nginx/sites-available && rm '{$project}'.conf && cd /etc/nginx/sites-enabled &&  rm '{$project}'.conf";
         }
         private function nginxConfigCreateAndLinkCmd($configData,$project){
                 return "cd /etc/nginx/sites-available && printf '{$configData}' > {$project}.conf && ln -s /etc/nginx/sites-available/{$project}.conf /etc/nginx/sites-enabled/ && sudo /usr/sbin/service nginx restart";
